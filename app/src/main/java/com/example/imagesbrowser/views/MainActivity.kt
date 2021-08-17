37a5f130b7f572be65e5266f3be6a9299286e07f
Marcin Gruchała <margruchala@gmail.com>
2021-08-17 11:28:36 +0200

    DI setup

--- app/src/main/java/com/example/imagesbrowser/views/MainActivity.kt
+++ app/src/main/java/com/example/imagesbrowser/views/MainActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import com.example.imagesbrowser.viewmodels.MainActivityViewModel
 import com.example.imagesbrowser.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val viewModel: MainActivityViewModel by viewModels()
