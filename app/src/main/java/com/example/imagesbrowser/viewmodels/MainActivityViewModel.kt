d14ffd2ec39d1ff76924459f557e5eab3a9af007
Marcin Gruchała <margruchala@gmail.com>
2021-08-17 11:08:05 +0200

    viewmodel setup

---
+++ app/src/main/java/com/example/imagesbrowser/viewmodels/MainActivityViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.imagesbrowser.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel : ViewModel() {
+}
--- app/src/main/java/com/example/imagesbrowser/views/MainActivity.kt
+++ app/src/main/java/com/example/imagesbrowser/views/MainActivity.kt
@@ -1,11 +1,14 @@
-package com.example.imagesbrowser
+package com.example.imagesbrowser.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import com.example.imagesbrowser.viewmodels.MainActivityViewModel
 import com.example.imagesbrowser.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainActivityViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
