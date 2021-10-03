a2b1185eef7fa79560dec895280561b731f8522f
Marcin Gruchała <margruchala@gmail.com>
2021-10-03 18:27:03 +0200

    Changed PiscumService

--- app/src/main/java/com/example/imagesbrowser/networking/service/PicsumService.kt
+++ app/src/main/java/com/example/imagesbrowser/networking/service/PicsumService.kt
@@ -1,6 +1,7 @@
 package com.example.imagesbrowser.networking.service
 
 import com.example.imagesbrowser.networking.model.ImagesListResponse
+import io.reactivex.rxjava3.core.Observable
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -11,5 +12,5 @@ interface PicsumService {
     suspend fun getImageLis(
         @Query("page") page: Int,
         @Query("limit") limit: Int
-    ) : Response<ImagesListResponse>
+    ) : Observable<ImagesListResponse>
 }
