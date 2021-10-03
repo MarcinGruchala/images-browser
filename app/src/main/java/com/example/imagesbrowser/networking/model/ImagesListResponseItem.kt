06257e7a6b503a914d0ea5683c4e99ab9e1e963c
Marcin Gruchała <margruchala@gmail.com>
2021-10-03 18:14:14 +0200

    Updated ImagesListResponseItem

--- app/src/main/java/com/example/imagesbrowser/networking/model/ImagesListResponseItem.kt
+++ app/src/main/java/com/example/imagesbrowser/networking/model/ImagesListResponseItem.kt
@@ -1,12 +1,24 @@
 package com.example.imagesbrowser.networking.model
 
-import java.io.Serializable
+import com.google.gson.annotations.SerializedName
 
 data class ImagesListResponseItem(
+
+    @SerializedName("author")
     val author: String,
-    val download_url: String,
+
+    @SerializedName("download_url")
+    val downloadUrl: String,
+
+    @SerializedName("height")
     val height: Int,
+
+    @SerializedName("id")
     val id: String,
+
+    @SerializedName("url")
     val url: String,
+
+    @SerializedName("width")
     val width: Int
-) : Serializable
+)
